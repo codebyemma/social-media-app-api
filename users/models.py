f4117ba92f9ebccf85a8cdbac9719264b533aabe
codebyemma <mysubbacc@gmail.com>
2025-12-25 09:36:47 +0100

    custom user model created

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    image = models.ImageField(upload_to='profile_images/', blank=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
